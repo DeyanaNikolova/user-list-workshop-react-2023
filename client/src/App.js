6b7e65403c78b571cd2c332759a0e40991c97df1
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-03-01 20:26:10 +0000

    Restructuring components

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,39 @@
-
 import './App.css'
+
 import CreateEdit from './components/CreateEdit';
 import Details from './components/Details';
 import Delete from './components/Delete';
 import Footer from './components/common/Footer';
 import Header from './components/common/Header';
-import UserContainer from './components/UserContainer';
+import Search from './components/search/Search';
+import UserSection from './components/user-section/UserSection';
+import Pagination from './components/Pagination';
 
 function App() {
-  return (
-    <div>
-    <Header />
-    <main className="main">
-      <UserContainer />
+    return (
+        <div>
+            <Header />
 
-     {/* <Details /> */}
+            <main className="main">
+                <section class="card users-container">
 
-     {/* <CreateEdit /> */}
+                    <Search />
 
-     {/* <Delete /> */}
+                    <UserSection />
+                    
+                    <Pagination />
 
-    </main>
-    <Footer />
-    </div>
-  );
+                    {/* <Details /> */}
+
+                    {/* <CreateEdit /> */}
+
+                    {/* <Delete /> */}
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/user-section/UserSection.js
+++ client/src/components/user-section/UserSection.js
@@ -8,7 +8,7 @@ export default function TableComponent() {
 
        {/* <div className="spinner"></div> */}
 
-        <div className="table-overlap">
+     {/*  <div className="table-overlap">
           <svg
             aria-hidden="true"
             focusable="false"
@@ -25,11 +25,11 @@ export default function TableComponent() {
             ></path>
           </svg>
           <h2>There is no users yet.</h2>
-        </div>
+  </div> */}
 
         {/*<!-- No content overlap component  -->*/}
 
-        <div className="table-overlap">
+         {/* <div className="table-overlap"> 
           <svg
             aria-hidden="true"
             focusable="false"
@@ -45,12 +45,12 @@ export default function TableComponent() {
               d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"
             ></path>
           </svg>
-          {/* <h2>Sorry, we couldn't find what you're looking for.</h2> */}
-        </div>
+           <h2>Sorry, we couldn't find what you're looking for.</h2> 
+        </div> */}
 
         {/* <!-- On error overlap component  -->  */}
 
-        <div className="table-overlap">
+        {/* <div className="table-overlap">
           <svg
             aria-hidden="true"
             focusable="false"
@@ -67,9 +67,8 @@ export default function TableComponent() {
             ></path>
           </svg>
            <h2>Failed to fetch</h2> 
-        </div>
-      </div>
-
+        </div> */}
+      </div> 
       <Table />
     </div>
   );
